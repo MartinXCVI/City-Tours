bb36eef7c81ce66277c001e46cfb33fb377767b5
MartinXCVI <mdzg96@gmail.com>
2024-01-12 23:20:02 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/Navbar'
+import './App.scss';
+import Tourlist from './components/Tourlist';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Tourlist />
+    </main>
+  );
+}
+
+export default App;
